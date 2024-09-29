333d53f76b583312a4e11a97af7358ce7528f769
Jayhind Yadav <jayhinddv@gmail.com>
2024-09-29 21:47:49 +0530

    fix:about

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import Home from "./pages/home";
 import About from "./pages/about";
 import Contact from "./pages/contact";
 import Education from "./pages/education";
-import More from "./pages/more";
 import Project from "./pages/project";
 import Skills from "./pages/skills";
 import { Route, Routes } from "react-router-dom";
@@ -15,7 +14,6 @@ export default function App() {
       <Route path="/skills" element={<Skills />} />
       <Route path="/projects" element={<Project />} />
       <Route path="/education" element={<Education />} />
-      <Route path="/more" element={<More />} />
       <Route path="/contact" element={<Contact />} />
     </Routes>
   );
