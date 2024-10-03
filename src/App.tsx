b6852aaf7fd82b2581b2f4ec0e8cd6a310f4ecfb
Jayhind Yadav <jayhinddv@gmail.com>
2024-10-03 22:34:40 +0530

    d

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Skills from "./pages/Skills";
-import Project from "./pages/Project";
-import Contact from "./pages/Contact";
-import Education from "./pages/Education";
+import Home from "./pages/Home.tsx";
+import About from "./pages/About.tsx";
+import Skills from "./pages/Skills.tsx";
+import Project from "./pages/Project.tsx";
+import Contact from "./pages/Contact.tsx";
+import Education from "./pages/Education.tsx";
 export default function App() {
   return (
     <Routes>
