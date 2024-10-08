af10c0487bdea69920103e3e5f12b1e657022117
Jayhind Yadav <jayhinddv@gmail.com>
2024-10-08 09:02:18 +0530

    f

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Ensure the output directory is set to 'dist'
+    outDir: 'dist',
   },
-  // If your app is served from a subpath, set the base accordingly
-  // base: '/your-subpath/',
+  base: '/', // Ensure base is set to '/'
 });
