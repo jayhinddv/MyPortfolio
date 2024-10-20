73b87da849a8e5d018654f16c64e8b264e843b47
Jayhind Yadav <jayhinddv@gmail.com>
2024-10-20 19:53:00 +0530

    d

--- src/components/DownLoadResumeBtn.tsx
+++ src/components/DownLoadResumeBtn.tsx
@@ -5,7 +5,7 @@ import { buttonVariants } from './ui/button'
 function DownLoadResumeBtn() {
   return (
     <div className="h-fit w-full mt-2 py-2 px-4">
-      <a href="/Resumes.pdf" className={cn(buttonVariants({ variant: "default", size: "lg" }))}>
+      <a href="/Resumes.pdf" target="_blank" className={cn(buttonVariants({ variant: "default", size: "lg" }))}>
         <Download className="mx-1" />
         Download Resume
       </a>
